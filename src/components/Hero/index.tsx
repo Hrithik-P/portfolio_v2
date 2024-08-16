194ce471a8941aaa3cad15e38433b16f825e934a
Hrithik P <hrithik00g@gmail.com>
2024-08-16 22:10:37 +0530

    feat: navigation bar added

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,24 +4,29 @@ import { HeroHighlight } from "../ui/HeroHighlight";
 import { Spotlight } from "../ui/Spotlight";
 
 import Skills from "../ui/Skills";
+import NavBar from "../NavBar";
 const Hero = () => {
   return (
     <HeroHighlight>
+      <NavBar />
       <div className="pb-20 pt-36 container">
         <div>
-          <Spotlight className="right-0 top-0 h-screen " fill="white" />
+          <Spotlight
+            className="lg:left-0 lg:top-0 h-screen -top-10 left-34 "
+            fill="white"
+          />
           <Spotlight
             className="top-10 left-full h-[80vh] w-[50vw]"
             fill="purple"
           />
           <Spotlight
-            className="top-28 left-120 h-[80vh] w-[50vw]"
+            className="top-60 left-150 h-[80vh] w-[50vw]"
             fill="blue"
           />
         </div>
         <div className=" flex justify-center items-center mx-10">
-          <div className="w-[60vw]">
-            <h1 className="font-bold lg:text-6xl md:text-5xl text-[40px]">
+          <div className="lg:w-[60vw] w-full">
+            <h1 className="font-bold lg:text-6xl md:text-5xl text-[40px] text-center lg:text-left">
               Hi, I’m Hrithik,
               <br />a creative{" "}
               <FlipWords
@@ -32,19 +37,19 @@ const Hero = () => {
               <br />
               developer
             </h1>
+            <div className="mt-5">
+              <p className="uppercase text-xs tracking-widest text-blue-100 max-w-[40vw] mb-10">
+                I bring value to web development projects by merging technical
+                expertise with creativity and aesthetics.
+              </p>
+              <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none ">
+                See my work
+              </button>
+            </div>
           </div>
-          <div className="w-[40vw]">
+          <div className="w-[40vw] hidden lg:block">
             <Skills />
           </div>
-        </div>
-        <div className="flex flex-col justify-center items-center mt-10">
-          <p className="uppercase text-center text-xs tracking-widest text-blue-100 max-w-[40vw] mb-10">
-            I bring value to web development projects by merging technical
-            expertise with creativity and aesthetics.
-          </p>
-          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none ">
-            See my work
-          </button>
         </div>
       </div>
     </HeroHighlight>
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FloatingNav } from "../ui/FloatingNav";
+
+const NavBar = () => {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      //   icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      //   icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      //   icon: (
+      //     <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      //   ),
+    },
+  ];
+  return <FloatingNav navItems={navItems} />;
+};
+
+export default NavBar;
